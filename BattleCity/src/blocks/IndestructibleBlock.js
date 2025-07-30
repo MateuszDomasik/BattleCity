f38d243d053f5a5da032567b0329c4d0579ed8cd
MaTTT805 <m.domasik@prosatis.pl>
2025-07-30 20:38:18 +0200

    Add wall image to indestructible block and fix palette alignment

--- BattleCity/src/blocks/IndestructibleBlock.js
+++ BattleCity/src/blocks/IndestructibleBlock.js
@@ -3,10 +3,20 @@ export class IndestructibleBlock {
     this.x = x;
     this.y = y;
     this.size = size;
+    this.type = 'indestructible';
+    this.destructible = false;
     this.color = 'purple';
+    this.image = new Image();
+    this.image.src = '../../assets/wall.png';
   }
-  render(ctx) {
+  draw(ctx) {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.size, this.size);
+    if (this.image.complete) {
+      ctx.drawImage(this.image, this.x, this.y, this.size, this.size);
+    }
+  }
+  render(ctx) {
+    this.draw(ctx);
   }
 } 
